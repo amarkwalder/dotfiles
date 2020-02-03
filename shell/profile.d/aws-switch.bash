c9a3d22fec3459214fbba86f52b6ae4e41d3493a
André Markwalder <andre.markwalder@gmail.com>
2020-02-03 03:40:20 +0100

    Added zsh support

---
+++ shell/profile.d/aws-switch.bash
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+function aws-switch() {
+    case "${1}" in
+        "" | clear)
+            export AWS_PROFILE=""
+            ;;
+        *)
+            export AWS_PROFILE="${1}"
+            ;;
+    esac
+}
+
